4f7fa79bec76512d90b19afd5755d88161927e15
theashrafqureshi-create <theashrafqureshi@gmail.com>
2026-07-10 09:49:11 +0300

    Update V2rayServiceManager.kt

--- app/src/main/kotlin/com/v2ray/ang/service/V2rayServiceManager.kt
+++ app/src/main/kotlin/com/v2ray/ang/service/V2rayServiceManager.kt
@@ -5,14 +5,26 @@ import android.content.Intent
 
 object V2rayServiceManager {
     fun startV2rayServer(context: Context) {
-        val intent = Intent(context, V2rayService::class.java)
-        intent.putExtra("COMMAND", "START")
-        context.startService(intent)
+        try {
+            val intent = Intent(context, Class.forName("com.v2ray.ang.service.V2rayService"))
+            intent.putExtra("COMMAND", "START")
+            context.startService(intent)
+        } catch (e: Exception) {
+            val intent = Intent(context, Class.forName("com.v2ray.ang.service.V2RayService"))
+            intent.putExtra("COMMAND", "START")
+            context.startService(intent)
+        }
     }
 
     fun stopV2rayServer(context: Context) {
-        val intent = Intent(context, V2rayService::class.java)
-        intent.putExtra("COMMAND", "STOP")
-        context.startService(intent)
+        try {
+            val intent = Intent(context, Class.forName("com.v2ray.ang.service.V2rayService"))
+            intent.putExtra("COMMAND", "STOP")
+            context.startService(intent)
+        } catch (e: Exception) {
+            val intent = Intent(context, Class.forName("com.v2ray.ang.service.V2RayService"))
+            intent.putExtra("COMMAND", "STOP")
+            context.startService(intent)
+        }
     }
 }
