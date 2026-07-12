5447dd62b8a8d827dd3f89a538738e364fe5c862
theashrafqureshi-create <theashrafqureshi@gmail.com>
2026-07-12 03:16:36 +0300

    Update AppConfig.kt

--- app/src/main/java/com/ashraf/whatsappvpn/AppConfig.kt
+++ app/src/main/java/com/ashraf/whatsappvpn/AppConfig.kt
@@ -1,17 +1,11 @@
 package com.ashraf.whatsappvpn
 
 import android.app.Application
-import com.tencent.mmkv.MMKV
-import com.github.shadowsocks.Core
 
 class AppConfig : Application() {
     override fun onCreate() {
         super.onCreate()
-        
-        // ऐप शुरू होते ही डेटा स्टोर (MMKV) को चालू करने के लिए
-        MMKV.initialize(this)
-        
-        // शैडोसॉक्स कोर इंजन को ऐप के साथ इनिशियलाइज करने के लिए
-        Core.init(this, MainActivity::class.java)
+        // पुरानी लाइब्रेरीज (MMKV और Core) वाला कोड यहाँ से हटा दिया गया है
+        // क्योंकि उनकी अब कोई ज़रूरत नहीं है।
     }
 }
