1ce62e77a6c46288547802b4d142749312bd651d
theashrafqureshi-create <theashrafqureshi@gmail.com>
2026-07-15 07:28:41 +0300

    Update MainActivity.java

--- app/src/main/java/com/ashraf/whatsappvpn/ui/MainActivity.java
+++ app/src/main/java/com/ashraf/whatsappvpn/ui/MainActivity.java
@@ -47,32 +47,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 🛠️ [CRITICAL FIX] क्रैश पकड़ने वाला हैंडलर यहाँ जोड़ दिया है
-        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
-            @Override
-            public void uncaughtException(Thread thread, Throwable throwable) {
-                // लॉग में एरर रिकॉर्ड करने के लिए
-                Log.e("APP_CRASH", "Crash detected: ", throwable);
-                
-                // स्क्रीन पर बड़ा सा एरर मैसेज (Toast) दिखाने के लिए ताकि मोबाइल पर दिख जाए
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Looper.prepare();
-                        Toast.makeText(getApplicationContext(), 
-                            "💥 APP CRASHED!\nError: " + throwable.toString(), 
-                            Toast.LENGTH_LONG).show();
-                        Looper.loop();
-                    }
-                }).start();
-
-                // मैसेज पढ़ने के लिए ऐप को 4 सेकंड होल्ड पर रखेगा, फिर बंद करेगा
-                try { Thread.sleep(4000); } catch (InterruptedException e) {}
-                android.os.Process.killProcess(android.os.Process.myPid());
-                System.exit(10);
-            }
-        });
-
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
                 requestPermissions(new String[]{Manifest.permission.POST_NOTIFICATIONS}, 101);
